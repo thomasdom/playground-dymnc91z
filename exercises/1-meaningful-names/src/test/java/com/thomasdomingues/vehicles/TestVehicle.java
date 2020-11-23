4ecec2cf9a5668713b0d1f7921fa247f43730fe8
Thomas Domingues <thomasdomingues@icloud.com>
2020-11-24 00:27:30 +0100

    :art: Add project maven structure

---
+++ exercises/1-meaningful-names/src/test/java/com/thomasdomingues/vehicles/TestVehicle.java
@@ -0,0 +1,11 @@
+package com.thomasdomingues.vehicles;
+
+import org.junit.Test;
+
+public class TestVehicle {
+    @Test
+    public void test() {
+        System.out.println(String.format("TECHIO> message --channel \"%s\" \"%s\"", "Info", "Nothing to test here."));
+        System.out.println(String.format("TECHIO> success true"));
+    }
+}
